2db099591c709dd7eab5c041496ce95579125544
Nathan Worden <worden.nathan@gmail.com>
2018-11-30 16:38:01 -0600

    Mortgage Calculator

--- lesson_2/13.assignment_mortgage_car_loan_calculator.rb
+++ lesson_2/13.assignment_mortgage_car_loan_calculator.rb
@@ -1,12 +1,68 @@
-puts "Hello! Welcome to the Mortgage Calculator!"
+def prompt(message)
+  Kernel.puts("=> #{message}")
+end
+
+def valid_apr?(apr)
+  apr.to_f.to_s == apr
+end
 
 
-puts "What is the loan amount?"
-loan_amount = gets.chomp
+prompt("Welcome to the Mortgage Calculator!")
 
-puts "What is the Annual Percentage Rate? (APR)"
-apr = gets.chomp
+prompt("What is the loan amount?")
 
-puts "What is the loan duration?"
-loan_duration = gets.chomp
+loan_amount = ''
+loop do
+  loan_amount = Kernel.gets().chomp()
+  if loan_amount.empty?() || loan_amount.to_f() < 0
+    prompt("Must enter positive number.")
+  else
+    break
+  end
+end
+
+
+  prompt("What is the Annual Percentage Rate? (APR)")
+  prompt("(Example: 5 for 5% or 2.5 for 2.5%)")
+
+  apr = ''
+  loop do 
+    apr = Kernel.gets().chomp()
+    if apr.empty?() || apr.to_f() < 0
+      prompt("Must enter positive number.")
+    else
+      break
+    end
+  end
+
+
+
+prompt("What is the loan duration in years?")
+loan_duration_years = ''
+loop do
+  loan_duration_years = Kernel.gets().chomp()
+  if loan_duration_years.empty?() || loan_duration_years.to_i() < 0
+    prompt("Enter a valid number")
+  else
+    break
+  end
+end
+
+apr_format_corrected = apr.to_f() / 100
+monthly_interest_rate = apr_format_corrected / 12
+prompt("your monthly interest rate is #{monthly_interest_rate}")
+loan_duration_months = loan_duration_years.to_i * 12
+prompt("your loan duration in months is #{loan_duration_months}")
+
+monthly_payment = loan_amount.to_f() * 
+                    (monthly_interest_rate / 
+                    (1 - (1 + monthly_interest_rate)**(-loan_duration_months)))
+
+prompt("Your monthly payment will be #{monthly_payment}")
+
+
+
+
+
+
 
