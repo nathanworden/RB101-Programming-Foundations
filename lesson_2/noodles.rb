35da55643d1029f8cc5786127cf2f986d0fcf27f
Nathan Worden <worden.nathan@gmail.com>
2018-12-02 16:19:03 -0600

    noodles

--- lesson_2/noodles.rb
+++ lesson_2/noodles.rb
@@ -3,7 +3,7 @@ def prompt(message)
 end
 
 def valid_num?(num)
-  num =~ /^\d+$/
+  num =~ /^\d*\.{0,1}\d+$/
 end
 
 
@@ -12,9 +12,12 @@ num1 = ''
 loop do
   num1 = gets.chomp
   if valid_num?(num1)
+    prompt "That is a valid number. Good job."
     break
   else
     prompt "hmm... that doesn't look quite right. Please enter a VALID number"
   end
 end
+
+
 
