0077f266c8eeb7e0f1d040be3e92283c2f06bd12
Nathan Worden <worden.nathan@gmail.com>
2018-12-05 18:34:21 -0600

    temp

---
+++ 02.lesson_2_small_programs/temp.rb
@@ -0,0 +1,10 @@
+# puts "Type in your name"
+# name = gets.chomp
+# puts "Hey #{name}, give me all your money."
+
+puts "How old are you?"
+age = gets.chomp.to_i
+puts "In 10 years you will be #{age + 10}"
+puts "In 20 years you will be #{age + 20}"
+puts "In 30 years you will be #{age + 30}"
+puts "In 40 years you will be #{age + 40}"
