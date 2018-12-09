97e7eda22a8488de1475088e7ebf56a659ca995a
Nathan Worden <worden.nathan@gmail.com>
2018-12-09 09:03:55 -0600

    notes on the general approach to problem solving

---
+++ RB101-RB109_small_problems/00.the_general_approach_for_problem_solving.rb
@@ -0,0 +1,73 @@
+# The General Approach for Problem Solving
+
+# Small Code Challenge Problems
+# - 20 - 45 minutes
+# - typical solutions 10-40 Lines of Code
+# - used extensively in interviews for a reason
+#    - master of a language
+#    - logic / reasoning
+#    - communication
+# - not a skill that you "acquire and file away", but need a lot of practice
+
+# Understand the Problem
+# - requirements are explicit
+#   - take notes
+#   - the odd words problem
+# - requirements are not so explicit and need to be modeled
+#   - requirements are showed with examples
+#      - examples need to be described in computational words
+#      - diamond of stars
+#   - implicit knowledge needs to be captured
+#      - convert to explict rules, incomputational language
+#      - what centry is that
+#   - identifying and defining imporant terms and concepts
+#      - [queen attack (maybe an easy version?)](https://launchschoo.com/exercises/81d3afa6)
+#      - same row; same colum; especially same diagnal
+#   - ask questions to verify your understanding!
+
+# Examples / Test Cases
+# - Input / Output
+# - Test cases serve two purposes:
+#   - help 
+
+
+###############################################################################
+
+# Practice Problem
+# Consider a character set consisting of letters, a space, and a point. Words consist
+# of one or more, but at most 20 letters. An input text consists of one or more words
+# separated from each other by one or more spaces and terminated by 0 or more spaces
+# followed by a point. Input should be read from, and including, the first letter of
+# the first word, up to and incluing the terminating point. The output text is to be
+# produced such that successive words are separated by a single space with the last 
+# word being termindated by a single point. Odd words are copied in reverse order
+# while even words are merely echoed.
+
+# For example, the input string
+# "whats the matter with kansas."
+
+# becomes
+# "whats eht matter htiw kansas."
+
+# Understanding the Problem:
+
+#  - input:
+#    - 1 - many words
+#    - separated by: 1 - many spaces
+#    - terminated by:
+#        - 0-many spaces
+#        - followed by a point
+#    - characters: letter, space, point, up to 20 letters
+#    - need to handle failure?
+# - output
+#    - words: have the odd words reversed
+#    - separated by one space between words
+#    - terminated: 0 space, followed by a dot
+
+
+
+
+#
+
+
+
