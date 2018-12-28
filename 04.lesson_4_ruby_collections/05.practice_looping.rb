dd7cdf38b4d57cb2ac21334b8fba011e2838742c
Nathan Worden <worden.nathan@gmail.com>
2018-12-27 19:42:49 -0800

    practice_looping

---
+++ 04.lesson_4_ruby_collections/05.practice_looping.rb
@@ -0,0 +1,4 @@
+# Practice Looping
+# In order to further solidify your understanding of loops, it's important to practice the knowledge you've acquired so far. Begin by completing the Loops 1 set of exercises on the Exercises page. Once you've completed all the exercises in that set, complete Loops 2.
+
+# Even if you've completed these exercises before reaching this assignment, it's worthwhile to take the time and briefly review the concepts covered in each exercise. At this point, you should feel comfortable with all of the looping elements and you shouldn't have any trouble writing loops that fulfill various requirements.
