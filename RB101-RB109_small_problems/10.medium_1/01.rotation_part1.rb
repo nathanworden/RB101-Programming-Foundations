708cff307c68bfe054c3b5752c6406b029e93b70
Nathan Worden <worden.nathan@gmail.com>
2019-02-04 07:08:54 -0600

    medium_1

---
+++ RB101-RB109_small_problems/10.medium_1/01.rotation_part1.rb
@@ -0,0 +1,123 @@
+# PEDAC
+
+
+# Problem
+# Write a method that rotates an array by moving the first element to the end of the array.
+# The original array should not be modified.
+
+# Examples / Test Cases
+# rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+# rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+# rotate_array(['a']) == ['a']
+# rotate_array(['apple', 6292, 'banana', 98, 'tritip', '1']) == [6292, 'banana', 98, 'tritip', '1', 'apple']
+
+# x = [1, 2, 3, 4]
+# rotate_array(x) == [2, 3, 4, 1]   # => true
+# x == [1, 2, 3, 4]                 # => true
+
+# Data Structures
+# array, integer, string
+
+# Algorithm
+# define a method called 'rotate_array' which accepts an 'array' as an argument
+# initiate a new empty array called 'result'
+# From 1 up to the size of 'array' minus one, do the following:
+# Take the |index| numbered element of 'array' and push it into result
+# After you have done this, then push the first element of 'array' to result
+# return result
+
+# Code
+
+# def rotate_array(array)
+#   result = []
+#   1.upto(array.size - 1) {|index| result << array[index]}
+#   result << array[0]
+#   result
+# end
+
+
+def rotate_array(arr)
+  first_el = arr.shift
+  arr << first_el
+  arr
+end
+
+
+
+p rotate_array([7, 3, 5, 2, 9, 1]) == [3, 5, 2, 9, 1, 7]
+p rotate_array(['a', 'b', 'c']) == ['b', 'c', 'a']
+p rotate_array(['a']) == ['a']
+p rotate_array(['apple', 6292, 'banana', 98, 'tritip', '1']) == [6292, 'banana', 98, 'tritip', '1', 'apple']
+
+p x = [1, 2, 3, 4]
+p rotate_array(x) #== [2, 3, 4, 1]   # => true
+p x #== [1, 2, 3, 4]     # => true
+
+
+# Book Answer
+# def rotate_array(array)
+#   array[1..-1] + [array[0]
+# end
+
+# Further Exploration
+# Write a moethod that rotates a string instead of an array.
+# Do the same for integers.
+# You may use 'rotate_array' from inside your new method
+
+
+# def rotate_string(string)
+#   array = string.chars
+#   result = array[1..-1] + [array[0]]
+#   result.join
+# end
+
+# p rotate_string('normandyostritch') == 'ormandyostritchn'
+# p rotate_string('abc') == 'bca'
+# p rotate_string('a') == 'a'
+# p rotate_string('apple dog bark wag') == 'pple dog bark waga'
+
+# x = 'abc'
+# p rotate_string(x) == 'bca'  # => true
+# p x == 'abc'     # => true
+
+# # Further Further Exploration
+
+# def rotate_integer(integer)
+#   array = integer.to_s.chars
+#   result = array[1..-1] + [array[0]]
+#   result.join.to_i
+# end
+
+# p rotate_integer(567756) == 677565
+# p rotate_integer(123) == 231
+# p rotate_integer(1) == 1
+# p rotate_integer(475_829_4812) == 758_294_8124
+
+# x = 123
+# p rotate_integer(x) == 231  # => true
+# p x == 123     # => true
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ RB101-RB109_small_problems/10.medium_1/02.rotation_part2.rb
@@ -0,0 +1,50 @@
+# PEDAC
+
+
+# Problem
+# Write a method that can rotate the last 'n' digits of a number.
+# Rotating just 1 digit results in the original number being returned
+# You may use the rotate_array method from the previous exercise if you want
+# You may assume that n is always a positive integer.
+
+# Examples / Test Cases
+# rotate_rightmost_digits(735291, 1) == 735291
+# rotate_rightmost_digits(735291, 2) == 735219
+# rotate_rightmost_digits(735291, 3) == 735912
+# rotate_rightmost_digits(735291, 4) == 732915
+# rotate_rightmost_digits(735291, 5) == 752913
+# rotate_rightmost_digits(735291, 6) == 352917
+
+# Data Structure
+# integers, array, string
+
+# Algorithm
+# define a method called 'rotate_rightmost_digits' which accepts two integers as arguments
+# integer2 = 10 to the power of integer_2
+# Collect in an 'array' 'integer_1' divided by 'integer_2' as well as 'integer_1' modulo 'integer_2'
+# integer1 = integer1 * 10 to the power of integer_2
+# turn the second element of the array into a string
+# Turn the string into an array of its characters
+# swap the first character for the last character
+# Join the array back into a string
+# Turn the string into an integer
+# Add the integer to integer_1
+
+# Code
+
+def rotate_rightmost_digits(integer1, integer2)
+  place = 10 ** integer2
+  array = integer1.divmod(place)
+  right_digits = array[1].to_s.chars
+  rotated = right_digits[1..-1] + [right_digits[0]]
+  left_digits = array[0] * place
+  left_digits + rotated.join.to_i
+end
+
+
+p rotate_rightmost_digits(735291, 1) == 735291
+p rotate_rightmost_digits(735291, 2) == 735219
+p rotate_rightmost_digits(735291, 3) == 735912
+p rotate_rightmost_digits(735291, 4) == 732915
+p rotate_rightmost_digits(735291, 5) == 752913
+p rotate_rightmost_digits(735291, 6) == 352917
