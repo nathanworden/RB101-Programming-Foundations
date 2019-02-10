d034a9809c980d5793c25aff74233e5c4dce209c
Nathan Worden <worden.nathan@gmail.com>
2019-02-09 16:01:20 -0800

    Quiz

---
+++ 04.lesson_4_ruby_collections/12.quiz/01.rb
@@ -0,0 +1,20 @@
+# Question 1 1 / 1 Points Correct
+# What happens if we try to initialize a Hash that contains duplicate keys?
+
+# Example:
+
+# hsh = {a: 100, b: 200, c: 300, a: 400}
+# Try to answer without simply running the above code.
+
+# Your Answer
+# A
+# The hash is created containing all of the key-value pairs but a warning is raised.
+
+# B
+# An exception is raised and the hash is not created.
+
+# C
+# Both of the key value pairs for the duplicate keys are removed from the hash.
+
+# D
+# The value for the second occurrence of the duplicate key overwrites the value for the first occurrence.
