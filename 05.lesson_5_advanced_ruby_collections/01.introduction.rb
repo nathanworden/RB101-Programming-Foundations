0d87ac35238aa5c603a24c7d7b7a52d0b889c3a2
Nathan Worden <worden.nathan@gmail.com>
2019-02-16 11:10:24 -0600

    lesson_5

---
+++ 05.lesson_5_advanced_ruby_collections/01.introduction.rb
@@ -0,0 +1,19 @@
+# Give us your feedback
+# Introduction
+# In the previous lesson we took an in-depth look at Ruby collections. You should now feel comfortable with what collections are on a conceptual level as well as with working with them on a practical level, through looping and using iterative methods. If any of those areas still feel at all unclear it would be a good idea to review them again now before moving on.
+
+# In this lesson we will be building on what we learned in the previous lesson and exploring some more advanced aspects of working with collections in Ruby such as sorting, nested collections and working with blocks.
+
+# For this lesson, create a peer directory to your lesson 4 code. Perhaps call it lesson_5, and do all your work in this lesson in that directory.
+
+# work           <-- this is your course 1 git repo
+# |
+# +--lesson_2     <-- all lesson 2 code goes here
+# |
+# +--lesson_3     <-- all lesson 3 code goes here
+# |
+# +--lesson_4     <-- all lesson 4 code goes here
+# |
+# +--lesson_5     <-- all lesson 5 code goes here
+
+# As with the previous few lessons, you don't need to run Rubocop on code in this lesson.
