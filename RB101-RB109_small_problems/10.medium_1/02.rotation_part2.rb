e4addfbec839c3c4d9ea779d72f5560981eee5f8
Nathan Worden <worden.nathan@gmail.com>
2019-02-15 18:36:13 -0600

    modification

--- RB101-RB109_small_problems/10.medium_1/02.rotation_part2.rb
+++ RB101-RB109_small_problems/10.medium_1/02.rotation_part2.rb
@@ -48,6 +48,7 @@ p rotate_rightmost_digits(735291, 3) == 735912
 p rotate_rightmost_digits(735291, 4) == 732915
 p rotate_rightmost_digits(735291, 5) == 752913
 p rotate_rightmost_digits(735291, 6) == 352917
+p 
 
 
 
