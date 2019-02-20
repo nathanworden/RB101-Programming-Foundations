2dd1a8ab84066fb18d8c95285ea9c828078b1b73
Nathan Worden <worden.nathan@gmail.com>
2019-02-20 06:11:57 -0600

    lesson_5

--- 05.lesson_5_advanced_ruby_collections/05.practice_problems_sorting_nested_collections_and_working_with_blocks.rb
+++ 05.lesson_5_advanced_ruby_collections/05.practice_problems_sorting_nested_collections_and_working_with_blocks.rb
@@ -215,7 +215,7 @@ p arr
 # arr # => [4, [3, 8]]
 # The value of b did change because b is an array and we are modifying that array by assigning a new value at index 0 of that array.
 
-=end
+
 
 # Practice Problem 8
 
@@ -245,6 +245,207 @@ hsh.each do |_, value|
     end
   end
 end
+
+
+
+# Practice Problem 9
+
+# Given this data structure, return a new array of the same structure but with the sub arrays being ordered (alphabetically or numerically as appropriate) in descending order.
+
+arr = [['b', 'c', 'a'], [2, 1, 3], ['blue', 'black', 'green']]
+
+p(arr.map do |set|
+    set.sort do |a, b|
+      b <=> a
+    end
+  end)
+
+
+
+
+
+# Practice Problem 10
+
+# Given the following data structure and without modifying the original array, 
+# use the map method to return a new array identical in structure to 
+# the original but where the value of each integer is incremented by 1.
+
+array = [{a: 1}, {b: 2, c: 3}, {d: 4, e: 5, f: 6}]
+
+# My answer. Almost by WRONG because it returns each hash in its own array.
+
+# output = array.map do |hsh|
+#            hsh.map do |key, value|
+#                {key => value += 1}
+#             end
+#          end
+
+# p output
+
+
+# Book Answer
+
+p(array.map do |hsh|
+  incremented_hash = {}
+  hsh.map do |key, value|
+    incremented_hash[key] = value + 1
+  end
+  incremented_hash
+end)
+
+
+
+# Another Book Answer:
+
+# p(array.each_with_object([]) do |hsh, arr|
+#   incremented_hash = {}
+#   hsh.each do |key, value|
+#     incremented_hash[key] = value + 1
+#   end
+#   arr << incremented_hash
+# end)
+
+
+
+# Practice Problem 11
+# Given the following data structure use a combination of methods,
+#  including either the select or reject method, to return a new array
+#   identical in structure to the original but containing only the integers
+#   that are multiples of 3.
+
+# arr = [[2], [3, 5, 7], [9], [11, 13, 15, 21]]
+
+#My answer. Technically right but didn't need the transformed_array
+# p(arr.map do |element|
+#   transformed_array = []
+#   element.select {|int| transformed_array << int if int % 3 == 0}
+#   transformed_array
+# end)
+
+# Book Answer
+
+# p(arr.map do |element|
+#   element.select do |num|
+#     num % 3 == 0
+#   end
+# end)
+
+
+# This was a challenge for myself. I wanted to leave the integers that 
+# Where not multiples of three, but subitstitut out multipes of three
+# # with 'X'
+# p(arr.map do |element|
+#   transformed_array = []
+#   element.map do |int|
+#     if int % 3 == 0
+#       transformed_array << "X"
+#     else
+#       transformed_array << int
+#     end
+#   end
+#   transformed_array
+# end)
+
+
+
+# Practice Problem 12
+# Given the following data structure, and without using the 
+# Array#to_h method, write some code that will return a hash where 
+# the key is the first item in each sub array and the value is the second 
+# item.
+
+arr = [[:a, 1], ['b', 'two'], ['sea', {c: 3}], [{a: 1, b: 2, c: 3, d: 4}, 'D']]
+
+output = {}
+
+arr.each do |array|
+  output[array[0]] = array[1]
+end
+
+p output
+
+p arr.to_h
+
+
+
+
+# Practice Problem 13
+# Given the following data structure, return a new array containing the same
+#  sub-arrays as the original but ordered logically according to the numeric 
+#  value of the odd integers they contain.
+
+arr = [[1, 6, 7], [1, 4, 9], [1, 8, 3]]
+
+# The sorted array should look like this:
+
+[[1, 8, 3], [1, 6, 7], [1, 4, 9]]
+
+p(arr.sort_by do |array|
+  array.select do |num|
+    num.odd?
+  end
+end)
+
+
+
+
+
+# Practice Problem 14
+# Given this data structure write some code to return an array containing 
+# the colors of the fruits and the sizes of the vegetables. The sizes should 
+# be uppercase and the colors should be capitalized.
+
+hsh = {
+  'grape' => {type: 'fruit', colors: ['red', 'green'], size: 'small'},
+  'carrot' => {type: 'vegetable', colors: ['orange'], size: 'medium'},
+  'apple' => {type: 'fruit', colors: ['red', 'green'], size: 'medium'},
+  'apricot' => {type: 'fruit', colors: ['orange'], size: 'medium'},
+  'marrow' => {type: 'vegetable', colors: ['green'], size: 'large'},
+}
+# The return value should look like this:
+
+# [["Red", "Green"], "MEDIUM", ["Red", "Green"], ["Orange"], "LARGE"]
+
+
+p(hsh.map do |key, value|
+  if value[:type] == 'fruit'
+    value[:colors].map do |element|
+      element.capitalize
+    end
+  else
+    value[:size].upcase
+  end
+end)
+
+
+=end
+
+# Practice Problem 15
+# Given this data structure write some code to return an array which 
+# contains only the hashes where all the integers are even.
+
+arr = [{a: [1, 2, 3]}, {b: [2, 4, 6], c: [3, 6], d: [4]}, {e: [8], f: [6, 10]}]
+
+# p(arr.each_with_object([]) do |hsh, array|
+#     hsh.each_pair do |key, value|
+#       array << hsh if value.all? {|num| num.even?}
+#     end
+#   end)
+
+
+# p(arr.select do |hsh|
+#   values = {}
+#   hsh.each_pair do |key, value|
+#     if value.all? {|num| num.even?}
+#       values[key] = value 
+#     else
+#       false
+#     end
+#   end
+# end)
+
+
+
 
 
 
