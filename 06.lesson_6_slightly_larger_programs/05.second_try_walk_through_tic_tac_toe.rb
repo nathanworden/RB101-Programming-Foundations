ecf39009ad8309a0a0cf89fea4846085a3bfa223
Nathan Worden <worden.nathan@gmail.com>
2019-03-06 17:32:11 -0600

    lesson_6

--- 06.lesson_6_slightly_larger_programs/05.second_try_walk_through_tic_tac_toe.rb
+++ 06.lesson_6_slightly_larger_programs/05.second_try_walk_through_tic_tac_toe.rb
@@ -64,6 +64,7 @@ def detect_winner(brd)
   winning_lines = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] +  # rows
                   [[1, 4, 7], [2, 5, 8], [3, 6, 9]] +  # columns
                   [[1, 5, 9], [3, 5, 7]]               # diagnals
+                  binding.pry
   winning_lines.each do |line|
     if brd[line[0]] == PLAYER_MARKER &&
        brd[line[1]] == PLAYER_MARKER &&
