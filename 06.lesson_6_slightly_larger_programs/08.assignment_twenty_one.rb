e7985b116bcb22ac858f7eef59847fe1ee37ba81
Nathan Worden <worden.nathan@gmail.com>
2019-03-27 17:33:58 -0500

    twenty one

--- 06.lesson_6_slightly_larger_programs/08.assignment_twenty_one.rb
+++ 06.lesson_6_slightly_larger_programs/08.assignment_twenty_one.rb
@@ -8,6 +8,10 @@
 # 6. If dealer bust, player wins.
 # 7. Compare cards and declare winner.
 
+def prompt(msg)
+  puts "=> #{msg}"
+end
+
 def initialize_deck
   new_deck = []
   suits = ['Hearts', 'Diamonds', 'Clubs', 'Spades']
@@ -33,9 +37,9 @@ def initial_deal(deck, player)
   end
 end
 
-def value_of_hand(hand)
+def value_of(hand)
    values = hand.map do |card|
-      case card
+      case
       when card.start_with?('J', 'Q', 'K')
         10
       when card.start_with?(/\d/)
@@ -44,9 +48,48 @@ def value_of_hand(hand)
         "Ace!"
       end
     end
+    if values.include?("Ace!")
+      if values.select {|card| card != "Ace!"}.reduce(:+) > 10
+        values.map! do |card|
+          if card == "Ace!"
+            return 1
+          else
+            return card
+          end
+        end
+      else
+        values.map! do |card|
+          if card == "Ace!"
+            return 11
+          else
+            return card
+          end
+        end
+      end
+    end
+    values.reduce(:+)
+end
+
+def busted?(hand)
+  value_of(hand) > 21
 end
 
 def player_turn(deck, player_hand, dealer_hand)
+  answer = nil
+  loop do
+    prompt "Would you like to hit, or stay?"
+    prompt "Current player hand: #{player_hand}"
+    answer = gets.chomp
+    hit(deck, player_hand) if answer.start_with?('h')
+    break if answer.start_with?('s') || busted?(player_hand)
+  end
+
+  if busted?(player_hand)
+    prompt "You busted! Game over"
+    return
+  else
+    prompt "You chose to stay!"
+  end
 
 
 end
@@ -58,14 +101,14 @@ deck = initialize_deck
 initial_deal(deck, player_hand)
 initial_deal(deck, dealer_hand)
 
+prompt "player hand: #{player_hand}"
+prompt "dealer hand: #{dealer_hand[0]}"
+
 player_turn(deck, player_hand, dealer_hand)
 
-puts "player hand: #{player_hand}"
-puts "dealer hand: #{dealer_hand[0]}"
+prompt player_hand
 
-
-
-p deck.count
+prompt "Number of cards left in deck: #{deck.count}"
 
 
 
