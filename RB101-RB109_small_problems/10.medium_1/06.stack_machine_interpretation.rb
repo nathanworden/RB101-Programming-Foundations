dc71e93a00227a9cbfb7a4e18a8350a63dd085e9
Nathan Worden <worden.nathan@gmail.com>
2019-04-24 20:36:23 -0500

    medium 1

--- RB101-RB109_small_problems/10.medium_1/06.stack_machine_interpretation.rb
+++ RB101-RB109_small_problems/10.medium_1/06.stack_machine_interpretation.rb
@@ -69,9 +69,9 @@ def minilang(string)
       when "MULT"
         register *= stack.pop
       when "DIV"
-        register = stack.pop / register
+        register /= stack.pop
       when "MOD"
-        register = stack.pop % register
+        register %= stack.pop
       when "POP"
         register = stack.pop
       when "PRINT"
@@ -84,11 +84,37 @@ end
 
 
 
-minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
+# minilang('PRINT')
+# 0
+
+# minilang('5 PUSH 3 MULT PRINT')
+# 15
+
+# minilang('5 PRINT PUSH 3 PRINT ADD PRINT')
 # 5
 # 3
 # 8
 
+# minilang('5 PUSH POP PRINT')
+# 5
+
+# minilang('3 PUSH 4 PUSH 5 PUSH PRINT ADD PRINT POP PRINT ADD PRINT')
+# 5
+# 10
+# 4
+# 7
+
+# minilang('3 PUSH PUSH 7 DIV MULT PRINT ')
+# 6
+
+# minilang('4 PUSH PUSH 7 MOD MULT PRINT ')
+# # 12
+
+# minilang('-3 PUSH 5 SUB PRINT')
+# 8
+
+# minilang('6 PUSH')
+# (nothing printed; no PRINT commands)
 
 
 
