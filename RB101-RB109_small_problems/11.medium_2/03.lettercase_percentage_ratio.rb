740bfa1230ecccc3d174beacd84c62933d09ab39
Nathan Worden <worden.nathan@gmail.com>
2019-05-02 18:56:38 -0500

    study

--- RB101-RB109_small_problems/11.medium_2/03.lettercase_percentage_ratio.rb
+++ RB101-RB109_small_problems/11.medium_2/03.lettercase_percentage_ratio.rb
@@ -37,16 +37,49 @@
 def letter_percentages(string)
   percentages = {}
   length = string.length.to_f
-  percentages[:lowercase] = ((string.count('a-z') / length) * 100).round(1)
-  percentages[:uppercase] = ((string.count('A-Z') / length) * 100).round(1)
-  percentages[:neither] = ((string.count('^a-z', '^A-Z') / length) * 100).round(1)
+  percentages[:lowercase] = (string.count('a-z') / length * 100).round(2)
+  percentages[:uppercase] = (string.count('A-Z') / length * 100).round(2)
+  percentages[:neither] = (string.count('^a-z', '^A-Z') / length * 100).round(2)
   percentages
 end
+
+
+
+# def letter_percentages(string)
+#   counts = { lowercase: 0, uppercase: 0, neither: 0 }
+#   percentages = { lowercase: [], uppercase: [], neither: [] }
+#   characters = string.chars
+#   length = string.length
+
+#   counts[:uppercase] = characters.count { |char| char =~ /[A-Z]/ }
+#   counts[:lowercase] = characters.count { |char| char =~ /[a-z]/ }
+#   counts[:neither] = characters.count { |char| char =~ /[^A-Za-z]/ }
+
+#   calculate(percentages, counts, length)
+
+#   percentages
+# end
+
+# def calculate(percentages, counts, length)
+#   percentages[:uppercase] = (counts[:uppercase] / length.to_f) * 100
+#   percentages[:lowercase] = (counts[:lowercase] / length.to_f) * 100
+#   percentages[:neither] = (counts[:neither] / length.to_f) * 100
+# end
+
+
 
 p letter_percentages('abCdef 123') == { lowercase: 50, uppercase: 10, neither: 40 }
 p letter_percentages('AbCd +Ef') == { lowercase: 37.5, uppercase: 37.5, neither: 25 }
 p letter_percentages('123') == { lowercase: 0, uppercase: 0, neither: 100 }
-p letter_percentages('Nathan Mark Worden has $40') == {lowercase: 61.5, uppercase: 11.5, neither: 26.9}
+p letter_percentages('Nathan Mark Worden has $40') == {lowercase: 61.54, uppercase: 11.54, neither: 26.92}
+p letter_percentages('abcdefGHI') == {:lowercase=>66.67, :uppercase=>33.33, :neither=>0.0}
+
+
+
+
+
+
+
 
 
 
---
+++ RB101-RB109_small_problems/11.medium_2/04.matching_parentheses.rb
@@ -0,0 +1,95 @@
+#PEDAC
+
+# Problem
+# Write a method that takes a string as an argument
+# Returns true if all parentheses in the string are properly balanced, false otherwise
+
+
+# Examples / Test Cases
+# balanced?('What (is) this?') == true
+# balanced?('What is) this?') == false
+# balanced?('What (is this?') == false
+# balanced?('((What) (is this))?') == true
+# balanced?('((What)) (is this))?') == false
+# balanced?('Hey!') == true
+# balanced?(')Hey!(') == false
+# balanced?('What ((is))) up(') == false
+# balanced?('This (is(.(very)) confus ((ing) ) ') == false
+
+
+# Data Structures
+# Strings, Arrays, maybe hashs
+
+# Algorithm
+# define a methods called 'balanced?' which accepts a string as an argument.
+# Iterate through the string and select all the '(' and ')' characters
+# If the array has an odd number of characters, return false
+# Iterate through the selected array, shifting the first character an popping the last
+# If the first character is '(' and the last is ')' denote with true
+# If not, return false
+# If you get trhough the whole array without any falses, return true
+
+
+# Code
+
+# def balanced?(string)
+#   selected = string.chars.select {|char| char == '(' || char == ')'}
+#   return false if selected.length.odd?
+#   return true if selected.empty?
+#   counter = 0
+#   selected.each do |char|
+#     counter += 1 if char == '('
+#     counter -= 1 if char == ')'
+#     return false if counter < 0
+#   end
+#   return true if counter == 0
+#   false
+# end
+
+
+# p balanced?('What (is) this?') == true
+# p balanced?('What is) this?') == false
+# p balanced?('What (is this?') == false
+# p balanced?('((What) (is this))?') == true
+# p balanced?('((What)) (is this))?') == false
+# p balanced?('Hey!') == true
+# p balanced?(')Hey!(') == false
+# p balanced?('What ((is))) up(') == false
+# p balanced?('((What)) )is( this()') == false
+
+
+i 
+
+
+# Further Exploration
+def balanced?(string)
+  parens = 0
+  square_brackets = 0
+  curly_brackets = 0
+  string.each_char do |char|
+    parens += 1 if char == '('
+    parens -= 1 if char == ')'
+    square_brackets += 1 if char == '['
+    square_brackets -= 1 if char == ']'
+    curly_brackets += 1 if char == '{'
+    curly_brackets -= 1 if char == '}'
+    break if parens < 0
+    break if square_brackets < 0
+    break if curly_brackets < 0
+  end
+
+  return true if parens.zero? && curly_brackets.zero? && curly_brackets.zero?
+  false
+end
+
+
+p balanced?('What [(is)] this?') == true
+p balanced?('What (is)} this?') == false
+p balanced?('What[] (is{} this?') == false
+p balanced?('((What) (is this){}[])?') == true
+p balanced?('(({{What))}} [(is this)])]?') == false
+p balanced?('Hey!') == true
+p balanced?('())}Hey!{()') == false
+p balanced?('What ((is))) up(){') == false
+p balanced?('((What)) [())is()] [this()') == false
+
