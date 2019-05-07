610933d81312ca9720eedddd49f2c921778373c3
Nathan Worden <worden.nathan@gmail.com>
2019-05-06 20:52:58 -0500

    medium 2

--- RB101-RB109_small_problems/11.medium_2/07.unlucky_days.rb
+++ RB101-RB109_small_problems/11.medium_2/07.unlucky_days.rb
@@ -35,7 +35,91 @@
 # Code
 
 def friday_13th(year)
-  
+  days = %w(Sunday Monday Tuesday Wednesday Thursday Friday Saturday)
+  days_in_month = {'January' => 31, 'February' => 28, 'March' => 31, 'April' => 30, 
+            'May' => 31, 'June' => 30, 'July' => 31, 'August' => 31, 
+            'September' => 30, 'October' => 31, 'November' => 30, 'December' => 31}
+  years = (1753...year).to_a
+  total_days = years.map do |year|
+    if leap_year?(year)
+      366
+    else
+      365
+    end
+  end
+  day = total_days.reduce(:+) % 7
+
+  jan_1st_of_year = day + 1
+
+  days_in_month['February'] = 29 if leap_year?(year)
+  first_of_month = [jan_1st_of_year]
+
+  carry_over = jan_1st_of_year
+
+  days_in_month.each do |month, days|
+    next if month == 'December'
+    first_of_month << (days + first_of_month[-1]) % 7
+  end
+  first_of_month.map! do |day|
+    days[day]
+  end
+  number_of_friday_13ths = first_of_month.select do |day|
+    day == 'Sunday'
+  end
+  number_of_friday_13ths.count
 end
+
+
+
+def leap_year?(year)
+  return true if year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
+  false
+end
+
+
+# p friday_13th(2015) == 3
+# p friday_13th(1986) == 1
+# p friday_13th(2019) == 2
+# p friday_13th(2020) == 2
+
+
+# Leap year?
+
+# p leap_year?(1804) == true
+# p leap_year?(1808) == true
+# p leap_year?(1812) == true
+# p leap_year?(1820) == true
+# p leap_year?(2000) == true
+# p leap_year?(2004) == true
+# p leap_year?(2005) == false
+# p leap_year?(1900) == false
+# p leap_year?(1753) == false
+
+
+
+require 'date'
+
+def friday_13th(year)
+  unlucky_count = 0
+  thirteenth = Date.new(year, 1, 13)
+  12.times do
+    unlucky_count += 1 if thirteenth.friday?
+    thirteenth = thirteenth.next_month
+  end
+  unlucky_count
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ RB101-RB109_small_problems/11.medium_2/08.next_featured_number_higher_than_a_given_value.rb
@@ -0,0 +1,13 @@
+# PEDAC
+
+# Problem
+
+featured(12) == 21
+featured(20) == 21
+featured(21) == 35
+featured(997) == 1029
+featured(1029) == 1043
+featured(999_999) == 1_023_547
+featured(999_999_987) == 1_023_456_987
+
+featured(9_999_999_999) # -> There is no possible number that fulfills those requirements
