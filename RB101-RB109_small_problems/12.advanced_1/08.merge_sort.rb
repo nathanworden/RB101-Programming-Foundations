4e2c94bb653af8bfef472dac47ec281dd885b942
Nathan Worden <worden.nathan@gmail.com>
2019-05-14 05:40:13 -0500

    advanced_1

--- RB101-RB109_small_problems/12.advanced_1/08.merge_sort.rb
+++ RB101-RB109_small_problems/12.advanced_1/08.merge_sort.rb
@@ -10,14 +10,70 @@ def merge_sort(array)
     return array
   end
 
-  half = array.size / 2
-  halfs = array.each_slice(half).to_a
+  # Apply "Divide & Conquer" strategy
 
-  if halfs.all? {|sub_arr| sub_arr.size == 1}
+  # 1. Divide
+  mid = array.size / 2
+  part_a = merge_sort(array.slice(0, mid))
+  part_b = merge_sort(array.slice(mid, array.size - mid))
 
-  else
-    halfs.each {|sub_arr| merge_sort(sub_arr)}
+
+# 2. Conquer
+  array = []
+  offset_a = 0
+  offset_b = 0
+  while offset_a < part_a.count && offset_b < part_b.count
+    a = part_a[offset_a]
+    b = part_b[offset_b]
+  
+    if a <= b
+      array << a
+      offset_a += 1
+    else
+      array << b
+      offset_b += 1
+    end
   end
+  
+  while offset_a < part_a.count
+    array << part_a[offset_a]
+    offset_a += 1
+  end
+  
+  while offset_b < part_b.count
+    array << part_b[offset_b]
+    offset_b += 1
+  end
+  
+  return array
 end
 
-p merge_sort([9, 5, 7, 1]) #== [1, 5, 7, 9]
+
+p merge_sort([9, 5, 7, 1]) == [1, 5, 7, 9]
+p merge_sort([5, 3]) == [3, 5]
+p merge_sort([6, 2, 7, 1, 4]) == [1, 2, 4, 6, 7]
+p merge_sort(%w(Sue Pete Alice Tyler Rachel Kim Bonnie)) == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
+p merge_sort([7, 3, 9, 15, 23, 1, 6, 51, 22, 37, 54, 43, 5, 25, 35, 18, 46]) == [1, 3, 5, 6, 7, 9, 15, 18, 22, 23, 25, 35, 37, 43, 46, 51, 54]
+
+
+
+
+
+
+# Level 1
+# [9, 5, 7, 1]
+# part_a = merge_sort([9, 5])
+# part_b = merge_sort([7, 1])
+
+    # Level 2-1                   # Level 2-2
+    # [9, 5]                        [7, 1]
+    # part_a = merge_sort([9])      part_a = merge_sort([7])
+    # part_b = merge_sort([5])      part_b = merge_sort([1])
+
+# Level 3-1     # Level 3-2        # Level 3-3      #Level 3-4
+# 9                 5                 7                1
+
+
+
+
+
---
+++ RB101-RB109_small_problems/12.advanced_1/08.merge_sort_book_answer.rb
@@ -0,0 +1,50 @@
+# Sort an array of passed in values using merge sort. You can assume that this array may contain only one type of data. And that data may be either all numbers or all strings.
+
+# Merge sort is a recursive sorting algorithm that works by breaking down the array elements into nested sub-arrays, then recombining those nested sub-arrays in sorted order.
+
+
+def merge(array1, array2)
+  index2 = 0
+  result = []
+
+  array1.each do |value|
+    while index2 < array2.size && array2[index2] <= value
+      result << array2[index2]
+      index2 += 1
+    end
+    result << value
+  end
+
+  result.concat(array2[index2..-1])
+end
+
+
+def merge_sort(array)
+  return array if array.size == 1
+
+  sub_array_1 = array[0...array.size / 2]
+  sub_array_2 = array[array.size / 2...array.size]
+
+  sub_array_1 = merge_sort(sub_array_1)
+  sub_array_2 = merge_sort(sub_array_2)
+
+  merge(sub_array_1, sub_array_2)
+end
+
+
+
+
+
+merge_sort([9, 5, 7, 1]) == [1, 5, 7, 9]
+merge_sort([5, 3]) == [3, 5]
+merge_sort([6, 2, 7, 1, 4]) == [1, 2, 4, 6, 7]
+merge_sort(%w(Sue Pete Alice Tyler Rachel Kim Bonnie)) == %w(Alice Bonnie Kim Pete Rachel Sue Tyler)
+merge_sort([7, 3, 9, 15, 23, 1, 6, 51, 22, 37, 54, 43, 5, 25, 35, 18, 46]) == [1, 3, 5, 6, 7, 9, 15, 18, 22, 23, 25, 35, 37, 43, 46, 51, 54]
+
+
+
+
+
+
+
+
